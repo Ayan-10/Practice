ff25db7399d148f771e3aa390b228c3f97c0572e
Ayan <ayansarkar1810@gmail.com>
2021-01-14 00:55:38 +0530

    gfg_stepnum

--- src/practice/steppingNumbers.java
+++ src/practice/steppingNumbers.java
@@ -14,13 +14,39 @@ public class steppingNumbers {
             String[] st = reader.readLine().trim().split("\\s+");
             int n = Integer.parseInt(st[0]);
             int m = Integer.parseInt(st[1]);
-            int ans = stepnum(n,m);
+            System.out.println(stepnum(n,m));
         }
     }
 
     private static int stepnum(int n, int m) {
         int ans = 0;
-        
+        for (int i = 0 ; i <= 9 ; i++) {
+            ans+=dfs(n, m, i, 0);
+        }
+
         return ans;
     }
+
+    private static int dfs(int n, int m, int i, int ans) {
+
+        if(i>=n && i<=m){
+            ans++;
+        }
+        if(i==0 || i>m){
+            return ans;
+        }
+        int ld = i%10;
+        int ia= i*10+(ld - 1);
+        int ib= i*10+(ld + 1);
+        if(ld==0){
+            ans+=dfs(n,m,ib,0);
+        }else if(ld==9){
+            ans+=dfs(n,m,ia,0);
+        }else{
+            ans+=dfs(n,m,ia,0);;
+            ans+=dfs(n,m,ib,0);
+        }
+        return ans;
+    }
+
 }
